2345768edb5536a3f81d85b4be493b0957bdabec
salboaie <salboaie@gmail.com>
2018-06-27 17:39:33 +0300

    wip csb stuff

--- bin/pskwallet.js
+++ bin/pskwallet.js
@@ -7,8 +7,13 @@ require("callflow");
 //global function addCommand
 addCommand = function addCommand(verb, adverbe, funct, helpLine){
     var cmdId;
+    if(!helpLine){
+        helpLine = " ";
+    } else {
+        helpLine = " " + helpLine;
+    }
     if(adverbe){
-        cmdId = verb + " & " +  adverbe;
+        cmdId = verb + " " +  adverbe;
         helpLine = verb + " " +  adverbe + helpLine;
     } else {
         cmdId = verb;
@@ -19,8 +24,9 @@ addCommand = function addCommand(verb, adverbe, funct, helpLine){
 }
 
 function doHelp(){
+    console.log("List of commands:");
     for(var l in commands_help){
-        console.log(commands_help[l]);
+        console.log("\t", commands_help[l]);
     }
 }
 
@@ -31,30 +37,36 @@ addCommand("help", null, doHelp);
 
 function runCommand(){
   var argv = process.argv;
-  var cmdId = "help";
-  console.log(argv.length);
+  var cmdId = null;
   var cmd = null;
   argv.shift();
   argv.shift();
+
   if(argv.length >=1){
-      cmdId = argv[1];
+      cmdId = argv[0];
       cmd = commands[cmdId];
       argv.shift();
   }
 
+
   if(!cmd && argv.length >=1){
-      cmdId = argv[3] + "&" + argv[4];
+      cmdId = cmdId + " " + argv[0];
       cmd = commands[cmdId];
+      argv.shift();
   }
+
   if(!cmd){
-    console.log("Unkwnown command: ", cmdId);
-    cmd = "use";
-  } else {
-      cmd(argv);
+    if(cmdId){
+        console.log("Unknown command: ", cmdId);
+    }
+    cmd = doHelp;
   }
+
+  cmd(argv);
+
 }
 
-$$.__global.__loadLibrayRoot = __dirname + "/../libraries/";
+$$.__global.__loadLibraryRoot = __dirname + "/../libraries/";
 
 $$.requireLibrary("cmds");
 
--- libraries/cmds/csb.js
+++ libraries/cmds/csb.js
@@ -1,7 +1,11 @@
 
-/*
-    addCommand("set", "pin", doSetPin); //seteaza la csb-ul master
-    addCommand("create", "csb", doAddCSB); //creaza un nou CSB si il adaugi in csb-ul master
-    addCommand("key", "set", doSetKey); //seteaza o cheie intr-un csb
-    addCommand("key", "get", doGetKey); //seteaza o cheie intr-un csb
-*/
+function dummy(){
+    console.log("Executing dummy command ", arguments);
+}
+
+
+addCommand("set", "pin", dummy, "<newPin> <oldPin>"); //seteaza la csb-ul master
+addCommand("create", "csb", dummy, "<csbAlias>"); //creaza un nou CSB si il adaugi in csb-ul master
+addCommand("key", "set", dummy, "<keyName> <value>"); //seteaza o cheie intr-un csb
+addCommand("key", "get", dummy, "<keyName> "); //citeste o cheie intr-un csb
+
