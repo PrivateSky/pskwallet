40ae4cbf13f192c6d0f8f450916a5f7c24c983f5
Acer 2 <sava.dumitru.daniel@gmail.com>
2018-07-11 09:48:29 +0300

    set pin command

--- libraries/cmds/csb.js
+++ libraries/cmds/csb.js
@@ -4,14 +4,27 @@ var utils = require('../utils/utils');
 var path = require('path');
 
 
+function setPinRecursive(newPin, oldPin, aliasCsb) {
+	var csb = utils.readCSB(oldPin, aliasCsb);
+	if(!csb['csb']){
+		var dseedPath = path.join(utils.paths.auxFolder, aliasCsb + utils.extensions.dseed);
+		crypto.setPin(oldPin, newPin, dseedPath);
+		utils.writeCSB(newPin, aliasCsb, csb);
+	}else{
+		var childCSBs = csb['csb'];
+		for(var seed in childCSBs){
+			setPinRecursive(newPin, oldPin, childCSBs[seed]);
+		}
+	}
+}
 
-exports.doSetPin = function(oldPin, newPin){
-
-	var encryptedMaster = fs.readFileSync(utils.paths.masterCSB);
+doSetPin = function(newPin, oldPin){
 	var masterCSB = utils.readCSB(oldPin, 'master');
+	for(var s in masterCSB){
+		setPinRecursive(newPin, oldPin, masterCSB[s]);
+	}
 	crypto.setPin(oldPin, newPin, utils.paths.masterDseed);
 	utils.writeCSB(newPin, 'master', masterCSB);
-
 	console.log('setPin:');
 };
 
@@ -31,9 +44,29 @@ doAddCSB = function (pin, aliasCSB) {
 	utils.writeCSB(pin, aliasCSB, csb);
 };
 
+exports.doKeySet = function (pin, aliasCSB, keyName, value) {
+	var parentCsb = utils.readCSB(pin, aliasCSB);
+	if(keyName == 'csb'){
+		var seed = crypto.generateSeed();
+		for(var s in parentCsb){
+			if(parentCsb[s] == keyName){
+				throw new Error('A csb with the provided alias already exists');
+			}
+		}
+		var aliasChildCsb = Object.keys(value)[0];
+		utils.ensureDseedExists(pin, aliasChildCsb);
+		utils.writeCSB(pin, aliasChildCsb, value[aliasChildCsb]);
+		parentCsb[seed] = aliasChildCsb;
+
+	}else{
+		parentCsb[keyName] = value;
+	}
+	utils.writeCSB(pin, aliasCSB, parentCsb);
+
+};
 
 // addCommand("set", "pin", doSetPin); //seteaza la csb-ul master
-addCommand("create", "csb", doAddCSB, "<pin> <csbAlias>"); //creaza un nou CSB si il adaugi in csb-ul master
+// addCommand("create", "csb", doAddCSB, "<pin> <csbAlias>"); //creaza un nou CSB si il adaugi in csb-ul master
 // addCommand("key", "set", doSetKey); //seteaza o cheie intr-un csb
 // addCommand("key", "get", doGetKey); //seteaza o cheie intr-un csb
 
@@ -42,8 +75,8 @@ addCommand("create", "csb", doAddCSB, "<pin> <csbAlias>"); //creaza un nou CSB s
 // }
 
 
-// addCommand("set", "pin", dummy, "<newPin> <oldPin>"); //seteaza la csb-ul master
-// addCommand("create", "csb", dummy, "<csbAlias>"); //creaza un nou CSB si il adaugi in csb-ul master
-// addCommand("key", "set", dummy, "<keyName> <value>"); //seteaza o cheie intr-un csb
-// addCommand("key", "get", dummy, "<keyName> "); //citeste o cheie intr-un csb
+addCommand("set", "pin", doSetPin, "<newPin> <oldPin>"); //seteaza la csb-ul master
+addCommand("create", "csb", doAddCSB, "<pin> <csbAlias>"); //creaza un nou CSB si il adaugi in csb-ul master
+// addCommand("key", "set", dummy, "<pin> <csbAlias> <keyName> <value>"); //seteaza o cheie intr-un csb
+// addCommand("key", "get", dummy, "<pin> <csbAlias> <keyName> "); //citeste o cheie intr-un csb
 
