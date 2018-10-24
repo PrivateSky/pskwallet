a0cc100d35adbdf46ed990536a1c6acc538f06a2
StefanCosminR <stefancosmin.romanescu@gmail.com>
2018-10-24 16:34:33 +0300

    Added createRequestManager call

--- libraries/cmds/remotes.js
+++ libraries/cmds/remotes.js
@@ -1,58 +1,20 @@
-const configTransactionSwarm = {
-    meta: {
-        swarmName: 'swarm',
-        swarmId: 'remotes'
-    },
-    swarmName: "swarm"
-};
+const is = require("interact").createInteractionSpace();
+const is2 = require('interact').createRemoteInteractionSpace('testRemote', 'http://127.0.0.1:8080', 'localhost/agent/system');
 
-$$.flow.describe('setRemotes', {
-    set: function (alias, endpoint) {
+function doAddRemote(...args) {
+    console.error('args ', ...args);
+    is2.startSwarm('domain', 'initialize', args);
+}
 
-        const transaction = $$.blockchain.beginTransaction(configTransactionSwarm);
+function doGetRemotes(...args) {
 
-        const remotesSwarm = transaction.lookup('global.remote', 'remotes');
-        if (!remotesSwarm) {
-            // error
-            return;
+    is2.startSwarm('domain', 'initialize', args).on({
+        returnInfo: function(info) {
+            console.log(info);
         }
+    });
+}
 
-        remotesSwarm.init('remotes');
-        remotesSwarm.addRemote(alias, endpoint);
-        transaction.add(remotesSwarm);
 
-        console.log(remotesSwarm);
-
-        $$.blockchain.commit(transaction);
-    },
-    get: function (alias) {
-        const transaction = $$.blockchain.beginTransaction(configTransactionSwarm);
-
-        const swarm = transaction.lookup('global.remote', 'remotes');
-
-        if(alias) {
-            console.log(swarm.remotes[alias]);
-            return swarm.remotes[alias];
-        } else {
-            console.log(swarm.remotes);
-            return swarm.remotes;
-        }
-    }
-});
-
-const flow = $$.flow.start('setRemotes');
-
-const getSetter = function (key) {
-    return function (value) {
-        flow.set(key, value);
-    }
-};
-
-const getGetter = function (key) {
-    return function () {
-        flow.get(key);
-    }
-};
-
-addCommand("add", "remote", flow.set, "<alias> <endpoint>\t\t\t\t |add a new endpoint to local domain with alias");
-addCommand("get", "remotes", flow.get, "<endpoint>\t\t\t\t |get current endpoint for domain</endpoint>");
+addCommand("add", "remote", doAddRemote, "<alias> <endpoint>\t\t\t\t |add a new endpoint to local domain with alias");
+addCommand("get", "remotes", doGetRemotes, "<endpoint>\t\t\t\t |get current endpoint for domain</endpoint>");
