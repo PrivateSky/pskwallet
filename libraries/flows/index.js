4ffd614af753436524a711ed819330e471ad0480
skutner <sava.dumitru.daniel@gmail.com>
2019-02-13 10:10:12 +0200

    cleanup

--- libraries/flows/index.js
+++ libraries/flows/index.js
@@ -4,21 +4,14 @@ module.exports = $$.library(function () {
     require('./addCsb');
     require('./attachFile');
     require('./addPskdb');
-    require('./copyUrl');
     require('./createCsb');
-    require('./deleteUrl');
     require('./extract');
     require('./extractFile');
-    require('./getKey');
-    require('./getUrl');
     require('./listCSBs');
-    require('./moveUrl');
     require('./resetPin');
     require('./clone');
 	require('./saveBackup');
-    require('./setKey');
     require('./setPin');
-    require('./setUrl');
 });
 
 
