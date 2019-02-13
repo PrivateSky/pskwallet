083a7989661bea9d72af5f99c0c6f4b04a64a0f0
skutner <sava.dumitru.daniel@gmail.com>
2019-02-13 10:07:17 +0200

    cleanup

--- libraries/cmds/csb.js
+++ libraries/cmds/csb.js
@@ -64,13 +64,6 @@ function doCreateCsb(CSBPath) {
 	});
 }
 
-function doSetKey(aliasCsb, recordType, key, field) {
-	is.startSwarm("setKey", "start", aliasCsb, recordType, key, field).on({
-		readPin:readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	});
-}
 
 function doResetPin(){
 	is.startSwarm("resetPin", "start").on({
@@ -89,9 +82,6 @@ function doResetPin(){
 		printInfo: generateMessagePrinter(),
 		handleError: generateErrorHandler()
 	})
-}
-function doGetKey(aliasCsb, recordType, key, field) {
-	$$.flow.start("flows.getKey").start(aliasCsb, recordType, key, field);
 }
 
 function doSaveBackup(CSBPath) {
@@ -130,85 +120,14 @@ function doClone(CSBPath) {
 	});
 }
 
-function doSetUrl(url) {
-	is.startSwarm("setUrl", "start", url).on({
+function doAttachFile(url, filePath) {
+	is.startSwarm("attachFile", "start", url, filePath).on({
 		readPin: readPin,
-		confirmOverwrite: function (recordType, key, field, fields, overWrittenRecord) {
-			var self = this;
-
-			function generateConfirmationCb(){
-				return function (err, record) {
-					if(err){
-						$$.errorHandler.throwError(err);
-					}else{
-						self.swarm("addRecord", record, recordType, key, field);
-					}
-				}
-			}
-			if(field){
-				console.log("You are about to overwrite the following field:");
-			}else{
-				console.log("You are about to overwrite the following record:");
-			}
-			console.log(overWrittenRecord);
-			utils.confirmOperation("Do you want to continue?", null, function(err, rl){
-				if(field){
-					utils.enterField(field, rl, generateConfirmationCb());
-				}else{
-					utils.enterRecord(fields, 0, null, rl, generateConfirmationCb());
-				}
-
-			});
-		},
-		enterRecord: function (recordType, key, field, fields) {
-			var self = this;
-			utils.enterRecord(fields, 0, null, null, function (err, record) {
-				if(err){
-					throw err;
-				}
-				self.swarm("addRecord", record, recordType, key, field);
-			});
-		},
 		printInfo: generateMessagePrinter(),
 		handleError: generateErrorHandler()
-	});
+	})
 }
 
-function doGetUrl(url) {
-	is.startSwarm("getUrl", "start", url, function(err, result){
-		console.log(result);
-	}).on({
-		readPin: readPin,
-		handleError:generateErrorHandler(),
-		printRecord: function (record) {
-			console.log(record);
-			this.swarm("checkoutResult", record);
-		}
-	});
-}
-
-function doAddFile(csbUrl, filePath) {
-	is.startSwarm("addFile", "start", csbUrl, filePath).on({
-		readPin: readPin,
-		confirmOverwriteFile: function (filePath, csb, alias, indexAdiacent) {
-			var self = this;
-			console.log("A file with the name", path.basename(filePath), "already exists in the current csb");
-			var prompt = "Do you want to overwrite it ?";
-			utils.confirmOperation(prompt, null, function (err, rl) {
-				if(err){
-					throw err;
-				}
-				self.swarm("saveChildInCsb", filePath, csb, alias, indexAdiacent);
-			});
-		},
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	});
-}
-
-function doAddFolder(csbUrl, folderPath){
-	doAddFile(csbUrl, folderPath);
-}
 
 function doExtractFile(url){
 	is.startSwarm("extractFile", "start", url).on({
@@ -229,82 +148,11 @@ function doListCSBs(CSBPath) {
 	})
 }
 
-
-function doCopy(sourceUrl, destUrl) {
-	is.startSwarm("copy", "start", sourceUrl, destUrl).on({
-		readPin: readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	})
-}
-
-function doDelete(url) {
-	is.startSwarm("delete", "start", url).on({
-		readPin: readPin,
-		confirmDeletion: function (recordType) {
-			var self = this;
-			console.log("You are about to delete all records of type", recordType);
-			var prompt = "Do you want to proceed?";
-			utils.confirmOperation(prompt, null, function (err) {
-				if(err){
-					throw err;
-				}
-				self.swarm("deleteMultipleRecords");
-			});
-		},
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	})
-}
-
-function doMove(sourceUrl, destUrl) {
-	is.startSwarm("move", "start", sourceUrl, destUrl).on({
-		readPin: readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	})
-}
-
-function doAddPskdb(url) {
-	is.startSwarm("addPskdb", "start", url).on({
-		readPin: readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	});
-}
-
-function doAddFileToPskdb(url, filePath) {
-	is.startSwarm("attachFile", "start", url, filePath).on({
-		readPin: readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	})
-}
-
-function doExtractFromPskdb(url) {
-	is.startSwarm("extractTemp", "start", url).on({
-		readPin: readPin,
-		printInfo: generateMessagePrinter(),
-		handleError: generateErrorHandler()
-	})
-}
-
 addCommand("set", "pin", doSetPin,  "\t\t\t\t\t |change the pin"); //seteaza la csb-ul master
 addCommand("create", "csb", doCreateCsb, "<aliasCsb> \t\t\t\t |create a new CSB having the alias <aliasCsb>"); //creaza un nou CSB si il adaugi in csb-ul master
-addCommand("set", "key", doSetKey, "<aliasCsb> <recordType> <key> <field>   |set the key " ); //seteaza o cheie intr-un csb
-addCommand("get", "key", doGetKey, "<aliasCsb> <recordType> <key> <field>   |get the key " ); //citeste o cheie intr-un csb
 addCommand("save", "backup", doSaveBackup,"<url>\t\t\t\t |saveData all csbs at address <url>");
 addCommand("clone", null, doClone, "<alias>\t\t\t\t |restore the csb  or archive having the name <alias> from one \n\t\t\t\t\t\t\t  of the addresses stored in backup\n");
 addCommand("reset", "pin", doResetPin, "\t\t\t\t\t |enter the seed in order to set the pin to a new value");
-addCommand("set", "url", doSetUrl, "<url> \t\t\t\t\t |set/update the record/field pointed by the provided <url>");
-addCommand("get", "url", doGetUrl, "<url> \t\t\t\t\t |print the record/field indicated by te provided <url>");
-addCommand("add", "file", doAddFile, "<csbUrl> <filePath> \t\t\t |add a file to the csb pointed by <csbUrl>");
-addCommand("add", "folder", doAddFile, "<csbUrl> <folderPath> \t\t |add a folder to the csb pointed by <csbUrl>");
 addCommand("extract", "file", doExtractFile, "<csbUrl> <alias> \t\t\t |decrypt file/folder/csb having the alias <alias>, contained\n\t\t\t\t\t\t\t   by the csb pointed to by <csbUrl>\n");
 addCommand("list", "csbs", doListCSBs, "<aliasCsb> \t\t\t\t |show all child csbs in the csb <aliasCsb>; if <aliasCsb> \n\t\t\t\t\t\t\t  is not provided, the command will print all the csbs \n\t\t\t\t\t\t\t  in the current folder\n");
-addCommand("copy", null, doCopy, "<srcUrl> <destUrl> \t\t\t |copy the csb/record/field from <srcUrl> to <destUrl>");
-addCommand("delete", null, doDelete, "<url>\t\t\t\t\t |delete the csb/record/field pointed to by <url>");
-addCommand("move", null, doMove, "<srcUrl> <destUrl>\t\t\t |move the csb/record/field from <srcUrl> to <destUrl>");
-addCommand("add", "pskdb", doAddPskdb, "<url> \t\t\t |add a pskdb to csb pointed by <url>");
-addCommand("attach", "file", doAddFileToPskdb, "<url> <filePath>\t\t\t |add file <filepath> to pskdb pointed by <url>");
-addCommand("extra", "temp", doExtractFromPskdb, "<url> \t\t\t |extract asset referenced by <url> from pskdb inside of csb");
+addCommand("attach", "file", doAttachFile, "<url> <filePath>\t\t\t |add file <filepath> to pskdb pointed by <url>");
