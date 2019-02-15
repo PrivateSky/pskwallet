5a767dbc258d07adde1050d17702b336704c780e
skutner <sava.dumitru.daniel@gmail.com>
2019-02-15 13:50:20 +0200

    removed addPskdb.js

--- libraries/flows/index.js
+++ libraries/flows/index.js
@@ -3,7 +3,6 @@ require("callflow");
 module.exports = $$.library(function () {
     require('./addCsb');
     require('./attachFile');
-    require('./addPskdb');
     require('./createCsb');
     require('./extractFile');
     require('./listCSBs');
