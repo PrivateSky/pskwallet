40da67728b01ff69f7aafade7b39e53393eb8b41
skutner <sava.dumitru.daniel@gmail.com>
2019-03-20 15:42:18 +0200

    cleaning

--- libraries/flows/setPin.js
+++ libraries/flows/setPin.js
@@ -2,34 +2,31 @@ const validator = require("../../utils/validator");
 const DseedCage = require('../../utils/DseedCage');
 
 $$.swarm.describe("setPin", {
-	start: function (localFolder = process.cwd()) {
-		this.localFolder = localFolder;
-		this.swarm("interaction", "readPin", 3);
-	},
+    start: function (localFolder = process.cwd()) {
+        this.localFolder = localFolder;
+        this.swarm("interaction", "readPin", 3);
+    },
 
-	validatePin: function (oldPin, noTries) {
-		this.oldPin = oldPin;
-		validator.validatePin(this.localFolder, this, "interactionJumper", oldPin, noTries);
-	},
+    validatePin: function (oldPin, noTries) {
+        this.oldPin = oldPin;
+        validator.validatePin(this.localFolder, this, "interactionJumper", oldPin, noTries);
+    },
 
-	interactionJumper: function(){
-		this.swarm("interaction", "enterNewPin");
-	},
+    interactionJumper: function () {
+        this.swarm("interaction", "enterNewPin");
+    },
 
-	actualizePin: function (newPin) {
-		this.dseedCage = new DseedCage(this.localFolder);
-		console.log("old pin", this.oldPin);
-		this.dseedCage.loadDseedBackups(this.oldPin, validator.reportOrContinue(this, "saveDseed", "Failed to load dseed.", newPin));
-	},
+    actualizePin: function (newPin) {
+        this.dseedCage = new DseedCage(this.localFolder);
+        this.dseedCage.loadDseedBackups(this.oldPin, validator.reportOrContinue(this, "saveDseed", "Failed to load dseed.", newPin));
+    },
 
-	saveDseed: function (dseed, backups, pin) {
-		console.log("Bckups", backups);
-		console.log("Pin", pin);
-		console.log("Dseed", dseed);
-		this.dseedCage.saveDseedBackups(pin, dseed, backups, validator.reportOrContinue(this, "successState", "Failed to save dseed"));
-	},
+    saveDseed: function (dseed, backups, pin) {
 
-	successState: function () {
-		this.swarm("interaction", "printInfo", "The pin has been successfully changed.");
-	}
+        this.dseedCage.saveDseedBackups(pin, dseed, backups, validator.reportOrContinue(this, "successState", "Failed to save dseed"));
+    },
+
+    successState: function () {
+        this.swarm("interaction", "printInfo", "The pin has been successfully changed.");
+    }
 });
