a8f7a4157ffb94aac230b4ab2eff51b4ffbd49c2
skutner <sava.dumitru.daniel@gmail.com>
2019-04-01 17:23:21 +0300

    Implemented CSBIdentifier for referencing CSBs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports.loadWithPin   = RootCSB.loadWithPin;
 module.exports.writeNewMasterCSB = RootCSB.writeNewMasterCSB;
 module.exports.RootCSB = RootCSB;
 module.exports.RawCSB = require('./libraries/RawCSB');
+module.exports.CSBIdentifier = require('./libraries/CSBIdentifier');
 module.exports.Seed = require('./utils/Seed');
 module.exports.init = function () {
 	$$.loadLibrary("pskwallet", require("./libraries/flows/index"));
---
+++ libraries/CSBIdentifier.js
@@ -0,0 +1,251 @@
+const flowsUtils = require("../utils/flowsUtils");
+const crypto = require("pskcrypto");
+
+
+function CSBIdentifier(id, backupUrls, keyLen = 32) {
+    let seed;
+    let dseed;
+    let uid;
+    let encSeed;
+    let encDseed;
+
+    init(id);
+
+    this.getSeed = function () {
+        if(!seed){
+            throw new Error("Cannot return seed. Access is denied.")
+        }
+
+        return generateCompactForm(seed);
+    };
+
+    this.getDseed = function () {
+        if(dseed){
+            return generateCompactForm(dseed);
+        }
+
+        if(seed){
+            dseed = deriveSeed(seed);
+            return generateCompactForm(dseed);
+        }
+
+        throw new Error("Cannot return derived seed. Access is denied.")
+    };
+
+    this.getUid = function () {
+        if(uid){
+            return generateCompactForm(uid);
+        }
+
+        if(dseed){
+            uid = computeUid(dseed);
+            return generateCompactForm(uid);
+        }
+
+        if(seed){
+            dseed = deriveSeed(seed);
+            uid = computeUid(dseed);
+            return generateCompactForm(uid);
+        }
+
+        throw new Error("Cannot return uid");
+    };
+
+    this.getEncSeed = function (encryptionKey) {
+        if(encSeed){
+            return generateCompactForm(encSeed);
+        }
+
+        if(!seed){
+            throw new Error("Cannot return encSeed. Access is denied");
+        }
+
+        if (!encryptionKey) {
+            throw new Error("Cannot return encSeed. No encryption key was provided");
+        }
+
+        //TODO: encrypt seed using encryptionKey. Encryption algorithm remains to be chosen
+    };
+
+
+
+    this.getBackupUrls = function () {
+
+    };
+
+    //------------------------------ internal methods ------------------------------
+    function init() {
+        if (!id) {
+            if (!backupUrls) {
+                throw new Error("No backups provided.");
+            }
+
+            seed = create();
+        }else{
+            classifyId();
+        }
+    }
+
+    function classifyId() {
+        if(id && typeof id !== "string" && !Buffer.isBuffer(id)) {
+            throw new Error(`Id must be a string or a buffer. The type provided was ${typeof id}`);
+        }
+
+        const expandedId = load(id);
+        switch(expandedId.tag){
+            case 's':
+                seed = expandedId;
+                break;
+            case 'd':
+                dseed = expandedId;
+                break;
+            case 'u':
+                uid = expandedId;
+                break;
+            case 'es':
+                encSeed = expandedId;
+                break;
+            case 'ed':
+                encDseed = expandedId;
+                break;
+            default:
+                throw new Error('Invalid tag');
+        }
+    }
+
+
+
+    function create() {
+        let localSeed = {};
+        if (!Array.isArray(backupUrls)) {
+            backupUrls = [backupUrls];
+        }
+
+        localSeed.tag    = 's';
+        localSeed.random = crypto.randomBytes(keyLen);
+        localSeed.backup = backupUrls;
+
+        return localSeed;
+    }
+
+    function deriveSeed(seed) {
+        let compactSeed = seed;
+
+        if (typeof seed === 'object' && !Buffer.isBuffer(seed)) {
+            compactSeed = generateCompactForm(seed);
+        }
+
+        if (Buffer.isBuffer(seed)) {
+            compactSeed = seed.toString();
+        }
+
+        if (compactSeed[0] === 'd') {
+            throw new Error('Tried to derive an already derived seed.');
+        }
+
+        const decodedCompactSeed = decodeURIComponent(compactSeed);
+        const splitCompactSeed = decodedCompactSeed.substring(1).split('|');
+
+        const strSeed = Buffer.from(splitCompactSeed[0], 'base64').toString('hex');
+        const backupUrls = Buffer.from(splitCompactSeed[1], 'base64').toString();
+        const dseed = {};
+
+        dseed.tag = 'd';
+        dseed.random = crypto.deriveKey(strSeed, null, keyLen);
+        dseed.backup = JSON.parse(backupUrls);
+
+        return dseed;
+    }
+
+    function computeUid(dseed){
+        if(!dseed){
+            throw new Error("Dseed was not provided");
+        }
+
+        if (typeof dseed === "object" && !Buffer.isBuffer(dseed)) {
+            dseed = generateCompactForm(dseed);
+        }
+
+        const uid = {};
+        uid.tag = 'u';
+        uid.random = Buffer.from(crypto.generateSafeUid(dseed));
+
+        return uid;
+    }
+
+    function generateCompactForm({tag, random, backup}) {
+        let compactId = tag + random.toString('base64');
+        if (backup) {
+            compactId += '|' + Buffer.from(JSON.stringify(backup)).toString('base64');
+        }
+        return Buffer.from(encodeURIComponent(Buffer.from(compactId)));
+    }
+
+    function encrypt(id, encryptionKey) {
+        if(arguments.length !== 2){
+            throw new Error(`Wrong number of arguments. Expected: 2; provided ${arguments.length}`);
+        }
+
+        let tag;
+        if (typeof id === "object" && !Buffer.isBuffer(id)) {
+            tag = id.tag;
+            id = generateCompactForm(id);
+        }
+
+        if (tag === 's') {
+            //TODO encrypt seed
+        }else if (tag === 'd') {
+            //TODO encrypt dseed
+        }else{
+            throw new Error("The provided id cannot be encrypted");
+        }
+
+    }
+
+    function load(compactId) {
+        if(typeof compactId === "undefined") {
+            throw new Error(`Expected type string or Buffer. Received undefined`);
+        }
+
+        if(typeof compactId !== "string"){
+            compactId = compactId.toString();
+        }
+
+        const decodedCompactId = decodeURIComponent(compactId);
+        const id = {};
+        const splitCompactId = decodedCompactId.substring(1).split('|');
+
+        id.tag = decodedCompactId[0];
+        id.random = Buffer.from(splitCompactId[0], 'base64');
+
+        if(splitCompactId[1] && splitCompactId[1].length > 0){
+            id.backup = JSON.parse(Buffer.from(splitCompactId[1], 'base64').toString());
+        }
+
+        return id;
+    }
+
+    function isValidForm(id) {
+        try {
+            if (typeof id === 'string' || Buffer.isBuffer(id)) {
+                id = load(id);
+            }
+
+            if (id.tag !== 's' && id.tag !== 'd' && id.tag !== 'es' && id.tag !== 'ed' && id.tag !== 'u') {
+                return false;
+            }
+
+            if (!Buffer.isBuffer(id.random)) {
+                return false;
+            }
+
+            return !((id.tag === 's' || id.tag === 'd') && (!id.backup || id.backup.length === 0));
+
+        } catch (e) {
+            return false;
+        }
+    }
+
+}
+
+module.exports = CSBIdentifier;
