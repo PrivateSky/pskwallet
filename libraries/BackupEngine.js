5fa11731e285072dfd0f600c07eb80300f7cfbe7
skutner <sava.dumitru.daniel@gmail.com>
2019-04-09 16:47:02 +0300

    implemented BackupEngine for handling CSBs remote saving and loading

---
+++ libraries/BackupEngine.js
@@ -0,0 +1,208 @@
+function BackupEngineBuilder() {
+    const resolvers = {};
+    this.addResolver = function (name, resolver) {
+        resolvers[name] = resolver;
+    };
+
+    this.getBackupEngine = function(urls) {
+        if (!urls || urls.length === 0) {
+            throw new Error("No url was provided");
+        }
+
+        return new BackupEngine(urls, resolvers);
+    }
+}
+
+function BackupEngine(urls, resolvers) {
+
+    this.save = function (csbIdentifier, dataStream, callback) {
+        for (url of urls) {
+            resolverForUrl(url, (err, resolver) => {
+                if(err){
+                    return callback(err);
+                }
+
+                resolver.auth(url, undefined, (err) => {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    resolver.save(url, csbIdentifier, dataStream, callback);
+                });
+            });
+        }
+    };
+
+    this.load = function (csbIdentifier, version, callback) {
+        if (typeof version === "function") {
+            callback = version;
+            version = "";
+        }
+
+        tryDownload(csbIdentifier, version, 0, (err, resource) => {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(undefined, resource);
+        });
+    };
+
+    this.getVersions = function (csbIdentifier, callback) {
+
+    };
+
+    this.compareVersions = function (fileList, callback) {
+        for (url of urls) {
+            resolverForUrl(url, (err, resolver) => {
+                if (err) {
+                    return callback(err);
+                }
+
+                resolver.auth(url, undefined, (err) => {
+                    if (err) {
+                        return callback(err);
+                    }
+
+                    resolver.compareVersions(url, fileList, (err, modifiedFiles) => {
+                        if (err) {
+                            return callback(err);
+                        }
+
+                        callback(undefined, modifiedFiles);
+                    });
+                });
+
+            });
+        }
+    };
+
+    //------------------------------------------------ INTERNAL METHODS ------------------------------------------------
+
+    function resolverForUrl(url, callback) {
+        const keys = Object.keys(resolvers);
+        let resolver;
+        let i;
+
+        for (i = 0; i < keys.length; ++i) {
+            if (match(keys[i], url)) {
+                resolver = resolvers[keys[i]];
+                break;
+            }
+        }
+
+        if (i === keys.length) {
+            resolver = resolvers['evfs'];
+            if (!resolver) {
+                return callback(new Error(`No resolver matches the url ${url}`));
+            }
+        }
+
+        callback(undefined, resolver);
+    }
+
+    function match(str1, str2) {
+        return str1.includes(str2) || str2.includes(str1);
+    }
+
+    function tryDownload(csbIdentifier, version, index, callback) {
+        if (index === urls.length) {
+            return callback(new Error("Failed to download resource"));
+        }
+
+        const url = urls[index];
+        resolverForUrl(url, (err, resolver) => {
+            if (err) {
+                return callback(err);
+            }
+
+            resolver.auth(url, undefined, (err) => {
+                if (err) {
+                    return tryDownload(csbIdentifier, version, ++index, callback);
+                }
+
+                resolver.load(url, csbIdentifier, (err, resource) =>{
+                    if (err) {
+                        return tryDownload(csbIdentifier, version, ++index, callback);
+                    }
+
+                    callback(undefined, resource);
+                });
+            });
+
+        });
+    }
+}
+
+function EVFSResolver() {
+    let isAuthenticated = false;
+
+    this.auth = function (url, authObj, callback) {
+        isAuthenticated = true;
+        callback();
+    };
+
+    this.save = function(url, csbIdentifier, dataStream, callback) {
+        if (!isAuthenticated) {
+            return callback(new Error('Unauthenticated'));
+        }
+
+        $$.remote.doHttpPost(url + "/CSB/" + csbIdentifier.getUid(), dataStream, (err, res) => {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(undefined, res);
+        });
+    };
+
+    this.load = function(url, csbIdentifier, version, callback) {
+        if (!isAuthenticated) {
+            return callback(new Error('Unauthenticated'));
+        }
+
+        if (typeof version === "function") {
+            callback = version;
+            version = "";
+        }
+
+        $$.remote.doHttpGet(url + "/CSB/" + csbIdentifier.getUid() + "/" + version, (err, resource) => {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(undefined, resource);
+        });
+        
+    };
+
+    this.getVersions = function(url, csbIdentifier, callback) {
+        
+    };
+
+    this.compareVersions = function(url, filesList, callback) {
+        if (!isAuthenticated) {
+            return callback(new Error('Unauthenticated'));
+        }
+
+        $$.remote.doHttpPost(url + "/CSB/compareVersions", JSON.stringify(filesList), (err, modifiedFiles) => {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(undefined, modifiedFiles);
+        });
+    }
+}
+
+const engineBuilder = new BackupEngineBuilder();
+
+// engineBuilder.addResolver('dropbox', new DropboxResolver());
+// engineBuilder.addResolver('drive', new DriveResolver());
+engineBuilder.addResolver('evfs', new EVFSResolver());
+
+module.exports = {
+    getBackupEngine: function (urls) {
+        return engineBuilder.getBackupEngine(urls);
+    }
+};
