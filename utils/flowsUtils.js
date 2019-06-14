659bc4b2a36e919702db111b27fa788f8129ac6b
skutner <sava.dumitru.daniel@gmail.com>
2019-06-14 13:51:18 +0300

    removed paths

--- utils/flowsUtils.js
+++ utils/flowsUtils.js
@@ -5,8 +5,3 @@ exports.defaultBackup = "http://localhost:8080";
 exports.defaultPin = "12345678";
 exports.noTries = 3;
 
-exports.Paths = {
-	"auxFolder"          : path.join(process.cwd(), ".privateSky"),
-	"Dseed"             : path.join(process.cwd(), ".privateSky", "Dseed")
-};
-
