4e4f6f03dfe67b3713461bb7eb6252c5741b129c
StefanCosminR <stefancosmin.romanescu@gmail.com>
2020-01-29 16:06:39 +0200

    Implemented method to add an app folder inside a BAR

--- cmds/index.js
+++ cmds/index.js
@@ -7,13 +7,18 @@ function getEndpoint(){
     return endpoint;
 }
 
-function createArchive(folderPath){
-    const path = require("path");
+function getInitializedEDFS() {
     const EDFS = require("../../edfs");
     const endpoint = getEndpoint();
     const transportAlias = "pskwallet";
     $$.brickTransportStrategiesRegistry.add(transportAlias, new EDFS.HTTPBrickTransportStrategy(endpoint));
-    const edfs = EDFS.attach(transportAlias);
+    return EDFS.attach(transportAlias);
+}
+
+function createArchive(folderPath){
+    const path = require("path");
+    const edfs = getInitializedEDFS();
+
     edfs.createBarWithConstitution(path.resolve(folderPath), (err, archive)=>{
         if(err){
             throw err;
@@ -22,4 +27,28 @@ function createArchive(folderPath){
     });
 }
 
+function addApp(archiveSeed, appPath) {
+    if(!archiveSeed) {
+        throw new Error('Missing first argument, the archive seed');
+    }
+
+    if(!appPath) {
+        throw new Error('Missing the second argument, the app path');
+    }
+
+    const EDFS = require("../../edfs");
+    const edfs = getInitializedEDFS();
+
+    const bar = edfs.loadBar(archiveSeed);
+    bar.addFolder(appPath, EDFS.constants.CSB.APP_FOLDER, (err) => {
+        if(err) {
+            throw err;
+        }
+
+        console.log('All done');
+    })
+}
+
+
 addCommand("createArchive", null, createArchive, "<folderPath> \t\t\t\t |create an archive containing constitutions folder <folderPath> ");
+addCommand("addApp", null, addApp, " <archiveSeed> <folderPath> \t\t\t\t |add an app to an existing archive");
