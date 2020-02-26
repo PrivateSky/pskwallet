4f4791a52957c6bad3cb2af070167bf51be94230
skutner <sava.dumitru.daniel@gmail.com>
2020-02-26 14:17:36 +0200

    refactored insertPassword function

--- utils/consoleUtils.js
+++ utils/consoleUtils.js
@@ -1,40 +1,58 @@
-const readline = require("readline");
+const rl = "readline";
+const readline = require(rl);
 const getPassword = require("./getPassword").readPassword;
 
-function insertPassword(prompt, noTries, callback){
-	prompt = prompt || "Insert pin:";
-	if(noTries === 0){
-		console.log("You have inserted an invalid pin 3 times");
-		console.log("Preparing to exit");
-		process.exit(0);
-	}else {
-		getPassword(prompt, function (err, pin) {
-			if(err) {
-				console.log("You have inserted an invalid character");
-				console.log("Try again");
-				insertPassword(prompt, noTries-1, callback);
-			}else{
-				return callback(null, pin);
-			}
-		});
-	}
+const NO_TRIES = 3;
+const DEFAULT_PROMPT = "Insert pin:";
+
+function insertPassword(options, callback) {
+    if (typeof options === "function") {
+        callback = options;
+        options = {};
+    }
+
+    if (!callback) {
+        throw new Error("Misuse of function, reason: No callback given.");
+    }
+
+    options.prompt = options.prompt || DEFAULT_PROMPT;
+
+    if (typeof options.noTries === "undefined") {
+        options.noTries = NO_TRIES;
+    }
+
+    if (options.noTries === 0) {
+        return callback(new Error(`You have inserted an invalid pin ${NO_TRIES} times`));
+    } else {
+        getPassword(options.prompt,  (err, pin)=> {
+            if (err || (options.validationFunction && !options.validationFunction(pin))) {
+                if (options.noTries !== 1) {
+                    console.log("Validation failed. Maybe you have inserted an invalid character.");
+                    console.log("Try again");
+                }
+                options.noTries--;
+                insertPassword(options, callback);
+            } else {
+                return callback(null, pin);
+            }
+        });
+    }
 }
 
-function insertText(field, callback){
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
+function getFeedback(question, callback) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-	rl.question("Insert " + field + ":", (answer) => {
-		rl.close();
-		callback(null, answer);
-	});
+    rl.question(question, (answer) => {
+        rl.close();
+        callback(null, answer);
+    });
 }
-
 
 
 module.exports = {
-	insertPassword,
-	insertText,
+    insertPassword,
+    getFeedback,
 };
